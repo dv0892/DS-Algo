f01deaa7f4206c763740dc77f14872e724f64739
dvCodeIt <70142455+dvCodeIt@users.noreply.github.com>
2020-11-05 19:56:52 +0530

    Create LeetCode.1446.java

---
+++ test/app/string/LeetCode.1446.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxPower(String s) {
+        char[] input = s.toCharArray();
+        int max_cCh=1,cCh = 1;
+        for(int i=0; i < (input.length-1); i++){
+            if(input[i] == input[i+1])
+                cCh++;
+            else {
+                if(max_cCh < cCh) max_cCh = cCh;
+                
+                cCh = 1;
+            }
+        }
+        
+        return (max_cCh < cCh) ? cCh : max_cCh;
+    }
+}
