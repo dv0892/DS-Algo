ab4844013b7e911004ac6659aea066704144ca68
dvCodeIt <70142455+dvCodeIt@users.noreply.github.com>
2020-12-29 20:00:17 +0530

    Create LeetCode1656.java

---
+++ test/app/design/LeetCode1656.java
@@ -0,0 +1,29 @@
+class OrderedStream {
+
+    String[] arr;
+    int ptr;
+    
+    public OrderedStream(int n) {
+        arr = new String[n];
+        ptr = 0;
+    }
+    
+    public List<String> insert(int id, String value) {
+        
+        arr[id-1] =  value;
+        List<String> res = new ArrayList<>();
+        
+        while(ptr < arr.length && arr[ptr] != null){
+            res.add(arr[ptr++]);
+        }
+        
+        return res;
+        
+    }
+}
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream obj = new OrderedStream(n);
+ * List<String> param_1 = obj.insert(id,value);
+ */
