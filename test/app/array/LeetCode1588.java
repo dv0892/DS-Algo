a81796c20408e1ece5b730f095cbac68d3ee50d8
dvCodeIt <70142455+dvCodeIt@users.noreply.github.com>
2021-01-01 23:04:00 +0530

    Create LeetCode1588.java

---
+++ test/app/array/LeetCode1588.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        
+        int sum=0;
+        
+        for(int st=0;st<arr.length; st++){
+            for(int ed=st;ed<arr.length; ed++){
+                if( ((ed-st+1)%2) != 0)
+                for(int r=st;r<=ed;r++){
+                    sum += arr[r];
+                }
+            }
+        }
+        
+        return sum;
+    }
+   
+}
