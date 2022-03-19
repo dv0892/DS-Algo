e11444a00e9444a4dffc33e2650d43050ccc0ee9
unknown <dineshjmsbw25@gmail.com>
2022-03-19 13:19:35 +0530

    selection sort

--- test/app/sorts/Sorts.java
+++ test/app/sorts/Sorts.java
@@ -7,9 +7,10 @@ public class Sorts {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {4,2,6,3,1,9};
+		int[] arr = {4,2,6,6,1,9};
 		
-		bubbleSort(arr);
+		//bubbleSort(arr);
+		selectionSort(arr);
 		
 		System.out.println(Arrays.toString(arr));
 	}
@@ -33,8 +34,37 @@ public class Sorts {
 				if(a[j] > a[j+1])
 					Utils.swap(a, j+1, j);
 			}
-			
 		} 
+	}
+	
+	public static void selectionSort(int[] a) {
+		int n = a.length;
+		
+		/*
+		 * In selection sort, we can follow two strategies :
+		 * 1. Pick the largest element and place it at the end
+		 * 2. Or Pick the smallest element and place it at the start.
+		 */
+		
+		// Implementation for Point 1
+		/*for( int i = 0; i < (n); i++) {
+			int max_i = 0;
+			for( int j = 0; j < (n-i); j++) {
+				if(a[j] > a[max_i])
+					max_i = j;
+			}
+			Utils.swap(a, max_i, n-1-i);
+		} */
+		
+		// Implementation for Point 2
+		for( int i = 0; i < (n); i++) {
+			int min_i = i;
+			for( int j = min_i+1; j < (n); j++) {
+				if(a[j] < a[min_i])
+					min_i = j;
+			}
+			Utils.swap(a, min_i, i);
+		}
 	}
 
 }
