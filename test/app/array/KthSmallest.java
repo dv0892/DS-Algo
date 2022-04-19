dcf7ca1a1b9619e09e76a634c089bc84d7be211c
unknown <dineshjmsbw25@gmail.com>
2022-04-20 00:51:59 +0530

    Kth smallest

--- test/app/array/KthSmallest.java
+++ test/app/array/KthSmallest.java
@@ -15,10 +15,16 @@ public class KthSmallest {
     		arr[i] =  ThreadLocalRandom.current().nextInt(100);;
     	}
 		
-		System.out.println( Arrays.toString(arr) );
-		Arrays.sort(arr);
-		System.out.println( Arrays.toString(arr) );
-		int k = 9;
+		System.out.println(Arrays.toString(arr));
+		
+		/*
+		Here we have taken the task of  finding the
+		and even randomized k also.
+		The k will lie in the range of indices of length of array
+		*/
+		int k = ThreadLocalRandom.current().nextInt(0,arr.length);
+		
+		System.out.println(k );
 		System.out.println(new KthSmallest().kthSmallest(arr, 0, arr.length-1, k-1));
 	}
 	
@@ -27,15 +33,37 @@ public class KthSmallest {
 		if(p == q)
 			return arr[p];
 		
-		int  k = parition(arr,p,q);
-		
+		/*
+		 * 
+		 * 
+		 * This algo can be further optimized to contain
+		 * the case when there are duplicate elements
+		 * and our i lies in that range
+		 * 
+		 * int  k = parition(arr,p,q);
+
 		if( k == i )
 			return arr[k];
-		
+
 		if( i < k )
 			return kthSmallest(arr, p, k-1, i );
 		else
-			return kthSmallest(arr, k+1, q, k-i );
+			return kthSmallest(arr, k+1, q, i );*/
+		
+		
+		/*
+		 * This is a classic application of partition routine of
+		 * quick sort.
+		 */
+		Pair  pair = quickPartition_faster_randomized(arr,p,q);
+		
+		if( (i >= pair.i+1) && ( i <= (pair.j-1)) )
+			return arr[pair.i + (i-pair.i)];
+		
+		if( i <= pair.i )
+			return kthSmallest(arr, p, pair.i, i );
+		else
+			return kthSmallest(arr, pair.j, q, i );
 		
 	}
 	
@@ -44,7 +72,6 @@ public class KthSmallest {
 		int i=p-1;
 		int j=p;
 		
-		//int pivotIndex = ThreadLocalRandom.current().nextInt(p, q+1);
 		int pivot = arr[q];
 		
 		while( j < q) {
@@ -58,5 +85,27 @@ public class KthSmallest {
 		
 		return i;
 	}
+	
+	public static Pair  quickPartition_faster_randomized(int[] a, int l, int r) {
+    	
+    	int i = l-1; 
+    	int j = l;
+    	int k = r+1;
+    	
+    	int pIndex = ThreadLocalRandom.current().nextInt(l,r+1);
+    	int pivot = a[pIndex];
+    	
+    	
+    	while (j < k) {
+    		if(a[j] == pivot) {
+    			j++;
+    		} else if (a[j] < pivot)
+    			Utils.swap(a, ++i, j++);
+    		else
+    			Utils.swap(a, --k, j);
+    	}
+    	
+    	return new Pair(i,k);
+    }
 
 }
