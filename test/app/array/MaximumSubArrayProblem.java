5edcea1021c1e610169b25d50c8a181f9fc5440b
unknown <dineshjmsbw25@gmail.com>
2022-05-07 00:59:56 +0530

    LeetCode 53 Maximum Subarray Sum

--- test/app/array/MaximumSubArrayProblem.java
+++ test/app/array/MaximumSubArrayProblem.java
@@ -8,8 +8,11 @@ public class MaximumSubArrayProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Integer> A =  new ArrayList<>(Arrays.asList(-2,1,-3,4,-1,2,1,-5,4));
-		System.out.println(new MaximumSubArrayProblem().maxSubArray(A));
+		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		/*ArrayList<Integer> A =  new ArrayList<>(Arrays.asList(nums));
+		System.out.println(new MaximumSubArrayProblem().maxSubArray(A));*/
+		
+		System.out.println(Arrays.toString( new MaximumSubArrayProblem().maxSubArrayRes(nums)));
 	}
 	
 	public int maxSubArray(final List<Integer> A) {
@@ -23,6 +26,54 @@ public class MaximumSubArrayProblem {
 
         return global_max;
         
+    }
+	
+	
+	public int maxSubArray(int[] nums) {
+        
+        int t_max , max ;
+        t_max = max = nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+            t_max = Math.max(nums[i],nums[i]+t_max);
+            max   = Math.max(max,t_max);
+        }
+        
+        return max;
+    }
+	
+	public int[] maxSubArrayRes(int[] nums) {
+        
+        int t_max , max ;
+        t_max = max = nums[0];
+        
+        int j,k,l,m;
+        j = k = l = m = 0;
+        
+        for(int i=1;i<nums.length;i++){
+        	
+        	if( ( nums[i] + t_max ) > nums[i] ) {
+        		k++;
+        	} else {
+        		j=k=i;
+        	}
+        	
+            t_max = Math.max(nums[i],nums[i]+t_max);
+            
+            if(t_max > max) {
+            	l = j;
+            	m = k;
+            }
+            
+            max   = Math.max(max,t_max);
+        }
+        
+        int[] res = new int[m-l+1];
+        
+        for( int i=0; l <= m;)
+        	res[i++] = nums[l++];
+        
+        return res;
     }
 
 	
