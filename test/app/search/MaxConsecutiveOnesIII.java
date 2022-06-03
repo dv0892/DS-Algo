a14f553665425dc1d2e2f15aa3173c11b5c283d3
unknown <dineshjmsbw25@gmail.com>
2022-06-04 01:59:52 +0530

    Max Consective OnesIII

--- test/app/search/MaxConsecutiveOnesIII.java
+++ test/app/search/MaxConsecutiveOnesIII.java
@@ -4,8 +4,9 @@ public class MaxConsecutiveOnesIII {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
-		System.out.println( new MaxConsecutiveOnesIII().longestOnes(nums, 3));
+		//int[] nums = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+		int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+		System.out.println( new MaxConsecutiveOnesIII().longestOnesOptimized(nums, 10));
 	}
 	
 	public int longestOnes(int[] nums, int k) {
@@ -30,6 +31,41 @@ public class MaxConsecutiveOnesIII {
         }
         
         return ans;
+    }
+	
+public int longestOnesOptimized(int[] nums, int k) {
+        
+		// This is having O(n^2) complexity.
+		// Needs to optimized
+	
+		int s=0,e=0,f=k;
+		
+		//exhausting f
+		while( e < nums.length ) {
+			if( nums[e] == 0 ){
+    			if( f == 0)
+    				break;
+    			f--;
+    		}
+			e++;
+		}
+		
+		int ans = Integer.MIN_VALUE;
+		
+		// Now start traversing windows having k flipped zero's
+		while( e < nums.length ) {
+			if( nums[e] == 0 ){
+				ans = Math.max(ans, e - s );
+				
+    			if( nums[s] == 1 ) {
+    				while( nums[++s] == 1 );
+    			}
+    			s++;
+    		} 
+    			e++;
+		}
+		
+        return Math.max(ans, e-s );
     }
 
 }
