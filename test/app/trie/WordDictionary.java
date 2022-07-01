6be5ced1ac049cf71e666f3943cec2c94abe8f7f
unknown <dineshjmsbw25@gmail.com>
2022-07-01 22:50:53 +0530

    Trie Problems

--- test/app/trie/WordDictionary.java
+++ test/app/trie/WordDictionary.java
@@ -12,57 +12,73 @@ class WordDictionary {
 		dict.addWord("bad");
 		dict.addWord("dad");
 		dict.addWord("mad");
-		dict.addWord("ma");
+		//dict.addWord("ma");
 		
 		
-		System.out.println( dict.search("..") );
+		System.out.println( dict.search("..d") );
 	}
 	
 
-    Node root;
-    
-    public WordDictionary() {
-        root = new Node();
-    }
-    
-    public void addWord(String word) {
-        Node curr = root;
-        
-        for ( int i=0;i<word.length();i++) {
-            Node temp = curr.childrens.get( word.charAt(i) );
-            if ( temp == null ){
-                temp = new Node();
-                curr.childrens.put( word.charAt(i) , temp );
-            } 
-            curr = temp;
-        }
-        curr.endOfWord = true;
-    }
-    
-    public boolean search(String word) {
-        return search( root , word , 0);
-    }
-    
-    private boolean search ( Node curr , String word , int i ) {
-        
-        if( curr == null )
-            return false;
-        
-        if( i == word.length() ) {
-            return curr.endOfWord;
-        }
-        
-        if( word.charAt(i) != '.') {
-            return search( curr.childrens.get(word.charAt(i)) , word , i + 1); 
-        }  
-        
-        Set<Character> allChilds = curr.childrens.keySet();
-        for( char c : allChilds  ){
-            if( search( curr.childrens.get(c) , word , i + 1) )
-                return true;
-        }
-            
-        return false;
-    }
-}
+	TrieNode root;
+	    
+	    public WordDictionary() {
+	        root = new TrieNode();
+	    }
+	    
+	   public void addWord(String word) {
+		   TrieNode curr = root;
+	        
+	        for ( int i=0;i<word.length();i++) {
+	        	TrieNode temp = curr.getChildren(word.charAt(i));
+	            if ( temp == null ){
+	                temp = new TrieNode();
+	                curr.setChildren(word.charAt(i),temp);
+	            } 
+	            curr = temp;
+	        }
+	        curr.endOfWord = true;
+	    }
+	    
+	    public boolean search(String word) {
+	        return search( root , word , 0);
+	    }
+	    
+	    private boolean search ( TrieNode curr , String word , int i ) {
+	        
+	        if( curr == null )
+	            return false;
+	        
+	        if( i == word.length() ) {
+	            return curr.endOfWord;
+	        }
+	        
+	        if( word.charAt(i) != '.') {
+	            return search( curr.getChildren(word.charAt(i)) , word , i + 1); 
+	        }  
+	        
+	        for( TrieNode n : curr.childrens  ){
+	            if( search( n , word , i + 1) )
+	                return true;
+	        }
+	            
+	        return false;
+	    }
+	}
+
+	class TrieNode {
+		boolean endOfWord ;
+		TrieNode[] childrens;
+		
+	    TrieNode()	{
+	        childrens = new TrieNode[26];
+		}
+	    
+	    public TrieNode getChildren( char c ){
+	        return childrens[c-'a'];
+	    }
+	    
+	    public void setChildren( char c , TrieNode node ){
+	        childrens[c-'a'] = node ;
+	    }
+	}
 
