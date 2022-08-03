5ccd66a9ea6495ce01b2b9aeb63e6d3e79ec356e
unknown <dineshjmsbw25@gmail.com>
2022-08-03 18:23:57 +0530

    DP Problem

--- src/main/java/test/app/dp/CountSquareSubMatricesWithAllOnes.java
+++ src/main/java/test/app/dp/CountSquareSubMatricesWithAllOnes.java
@@ -6,7 +6,7 @@ public class CountSquareSubMatricesWithAllOnes {
 		// TODO Auto-generated method stub
        int[][] mat = {{0,1},{1,1}};
        
-       System.out.println( new CountSquareSubMatricesWithAllOnes().countSquares(mat));
+       System.out.println( new CountSquareSubMatricesWithAllOnes().countSquaresBottomUp(mat));
 	}
 	
 	public int countSquares(int[][] matrix) {
@@ -23,6 +23,32 @@ public class CountSquareSubMatricesWithAllOnes {
                 }
             }
             i++;
+        }
+        
+        return sum;
+    }
+	
+	public int countSquaresBottomUp(int[][] matrix) {
+        int sum = 0;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        
+        
+        
+        for( int r=rows-1;r>=0; r--){
+            for( int c=cols-1;c>=0; c--) {
+            	if( matrix[r][c] == 1) {
+            		
+            		dp[r][c] = 1 ;
+            		
+            		if( r+1 < rows && c+1 < cols )
+            			dp[r][c] += Math.min( dp[r+1][c+1], Math.min( dp[r+1][c], dp[r][c+1] ) );
+            		
+            	}
+            	
+            	sum += dp[r][c];
+            }
         }
         
         return sum;
