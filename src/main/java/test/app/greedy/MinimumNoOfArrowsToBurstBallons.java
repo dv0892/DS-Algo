0b9dce15a7e29a7129ede48de8e26e4afba6fd0c
unknown <dineshjmsbw25@gmail.com>
2022-08-28 23:40:30 +0530

    Greedy Problems

--- src/main/java/test/app/greedy/MinimumNoOfArrowsToBurstBallons.java
+++ src/main/java/test/app/greedy/MinimumNoOfArrowsToBurstBallons.java
@@ -11,23 +11,27 @@ public class MinimumNoOfArrowsToBurstBallons {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		//int[][] points = {{-2147483646,-2147483645},{2147483646,2147483647}};
+		int[][] points = {{-2147483646,-2147483645},{2147483646,2147483647}};
 		//System.out.println( Integer.MIN_VALUE);
 		//System.out.println( Integer.MAX_VALUE);
 		//System.out.println( System.in );
 		//int[][] points = {{3,9},{7,12},{3,8},{6,8},{9,10},{2,9},{0,9},{3,9},{0,6},{2,8}};
 		//int[][] points = {{2,-1},{2,3}};
 		
-		int[][] points = Utils.readLeetInput("leetinput");
+		//int[][] points = Utils.readLeetInput("leetinput");
 		System.out.println( new MinimumNoOfArrowsToBurstBallons().findMinArrowShots(points));
 	}
 	
 	
 	public int findMinArrowShots(int[][] points) throws FileNotFoundException {
        
+		
+		/*
+		 * All the time Spent in debug because I used wrong conditions in comparator.
+		 */
         //Arrays.sort( points ,(p1,p2) -> p1[0] == p2[0] ? ( p2[1] < p1[1] ? -1 : 0) : ( p1[0] < p2[0]  ? -1 : 0) );
         
-		Arrays.sort( points ,(p1,p2) -> ( p1[0] >= p2[0]  ? 0 : -1  ) );
+		Arrays.sort( points ,(p1,p2) -> Integer.compare(p1[0], p2[0]) );
 		
         PrintStream o = new PrintStream(new File("leetdebug"));
         
