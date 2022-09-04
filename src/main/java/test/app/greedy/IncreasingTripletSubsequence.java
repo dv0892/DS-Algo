eb933213f2c8e74f72bfc43c4ed934e9aefabcd1
unknown <dineshjmsbw25@gmail.com>
2022-09-05 00:06:29 +0530

    Greedy Problems

---
+++ src/main/java/test/app/greedy/IncreasingTripletSubsequence.java
@@ -0,0 +1,38 @@
+package test.app.greedy;
+
+public class IncreasingTripletSubsequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( new IncreasingTripletSubsequence().increasingTriplet( new int[] {
+		/*1,0,0,0,0,10,0,0,0,10000*/		
+		50,40,60,30,39,30,20,25,27}));
+	}
+	
+	 public boolean increasingTriplet(int[] nums) {
+	     
+         if( nums.length < 3 )
+        	 return false;
+         
+         int min1 , min2, pmin1 , pmin2 ;
+         pmin1 = pmin2 =min1 = min2 = nums[0];
+         
+         for ( int i=1;i<nums.length;i++) {
+        	 if( (pmin1 < pmin2 && pmin2 < nums[i]) || (min1 < min2 && min2 < nums[i]))
+        		 return true;
+        	 
+        	 if( nums[i] < min1 ) {
+        		 pmin1 = min1;
+        		 pmin2 = min2;
+        		 min1 = min2 = nums[i];
+        	 } else if( min1 == min2 && nums[i] > min1 ) {
+        		 min2 = nums[i];
+        	 } else if ( nums[i] > min1 && nums[i] <= min2 ) {
+        		 min2 = nums[i];
+        	 } 
+         }
+         
+		 return false;   
+	 }
+
+}
--- src/main/java/test/app/greedy/NonOverlappingIntervals.java
+++ src/main/java/test/app/greedy/NonOverlappingIntervals.java
@@ -1,10 +1,28 @@
 package test.app.greedy;
 
+import java.util.Arrays;
+
 public class NonOverlappingIntervals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
 	}
+	
+	public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(i1,i2)->Integer.compare(i1[1],i2[1]));
+        
+        int end = intervals[0][1];
+        int count = 0;
+        for( int i=1 ; i<intervals.length; i++) {
+        	if( intervals[i][0] < end ) {
+        		count++;
+        	} else
+        		end = intervals[i][1];
+        }
+        
+        return count;
+        
+   }
 
 }
